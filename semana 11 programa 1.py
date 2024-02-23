2665e7c24b343728cad50f64179769eb820523a3
LUCIA RODRIGUEZ <lj.rodriguezr@uea.edu.ec>
2024-02-23 11:24:10 -0500

    DEBERES ASIG. FUNDAMENTOS DE PROGRAMACION PROGRAMA 1

--- semana 11 programa 1.py
+++ semana 11 programa 1.py
@@ -1,18 +1,18 @@
 
 matriz = [
-    [10, 12, 13],
-    [14, 15, 16],
-    [17, 18, 19]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 ]
 
-no_fila =0
-busqueda = int(input("ingrese el valor de busqueda"))
+no_fila = 0
+busqueda = 6
 
 for fila in matriz:
     no_columna = 0
     for columna in fila:
         if (columna == busqueda):
             print(f" No. {busqueda} , posision es: [{no_fila}][{no_columna}]")
-no_columna += 1
-no_fila += 1
+        no_columna += 1
+    no_fila += 1
 
