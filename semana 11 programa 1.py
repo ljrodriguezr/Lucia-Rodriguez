0250996ed05b88a6c859dbebdbcad075f31e17c2
LUCIA RODRIGUEZ <lj.rodriguezr@uea.edu.ec>
2024-02-23 11:05:23 -0500

    DEBERES ASIG. FUNDAMENTOS DE PROGRAMACION PROGRAMA 1

--- semana 11 programa 1.py
+++ semana 11 programa 1.py
@@ -1,1 +1,18 @@
-print("hola mundo")
+
+matriz = [
+    [10, 12, 13],
+    [14, 15, 16],
+    [17, 18, 19]
+]
+
+no_fila =0
+busqueda = int(input("ingrese el valor de busqueda"))
+
+for fila in matriz:
+    no_columna = 0
+    for columna in fila:
+        if (columna == busqueda):
+            print(f" No. {busqueda} , posision es: [{no_fila}][{no_columna}]")
+no_columna += 1
+no_fila += 1
+
